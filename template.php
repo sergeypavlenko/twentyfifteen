3ab800d66b63dba2f34fba016b942d96e429777a
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2015-04-18 15:31:11 +0300

    Add style file.

--- template.php
+++ template.php
@@ -1,1 +1,20 @@
 <?php
+
+/**
+ * @file
+ * Process and preprocess for theme.
+ */
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function twentyfifteen_preprocess_html(&$variables) {
+  $viewport = array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width',
+    ),
+  );
+  drupal_add_html_head($viewport, 'viewport');
+}
