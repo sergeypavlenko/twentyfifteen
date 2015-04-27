5b7d5ac9283fe2c234a4d233813a8eab612b0a41
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2015-04-27 23:37:16 +0300

    Fixed bugs. Move google font to hook_css_alter().

--- js/twentyfifteen.js
+++ js/twentyfifteen.js
@@ -3,6 +3,8 @@
 
   Drupal.behaviors.twentyfifteenContent = {
     attach: function () {
+      document.documentElement.className = 'js';
+
       var articles = $('#block-twentyfifteen-content article.hentry');
 
       // Add class "hentry" for block system content if on page not havent
@@ -20,6 +22,11 @@
       // Hide comments block if then empty.
       if ($('.comments-area section div').length == 0) {
         $('.comments-area').hide();
+      }
+
+      // Remove margin bottom, if exists tabs.
+      if ($('#content main#main > .tabs').length > 0) {
+        $('#content main#main > header.page-header').css('margin-bottom', 0);
       }
     }
   };
